295c8afd4396852e8f67f35019f6030847ac51ed
nikkipeel <npeel3497@gmail.com>
2020-12-29 11:26:48 -0500

    added alt tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,7 +29,7 @@ export default function Home() {
     return (
         <main className="container flex flex-col bg-brown text-white">
             <section className="flex flex-col md:flex-row text-center md:text-left justify-center pt-12 lg:pt-36 px-8">
-            <img src="https://i.imgur.com/mz9QfKo.jpg" className="rounded-full mr-auto ml-auto md:mr-8 md:ml-0"></img>
+            <img src="https://i.imgur.com/mz9QfKo.jpg" className="rounded-full mr-auto ml-auto md:mr-8 md:ml-0" alt="portrait image"></img>
                 <div className="flex flex-col md:w-1/2">
                     <h1 className="mt-4 text-xl text-white font-bold mono leading-none lg:leading-snug">Hi there, I'm Nikki!</h1>
                     <p className="mt-4 text-base text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium eos quia nam repudiandae dolore rerum iste labore doloremque exercitationem doloribus dolorum quam inventore nostrum reiciendis voluptas harum, neque, ducimus eaque facilis, corrupti temporibus? Inventore, ipsa.</p>
